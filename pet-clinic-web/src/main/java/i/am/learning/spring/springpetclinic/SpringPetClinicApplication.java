5674da5698f9b3ccb6519bffe8e2226d28d8a904
Asmi <maredia1990@gmail.com>
2020-12-03 14:10:16 -0500

    Commiting only for mvn:release

--- pet-clinic-web/src/main/java/i/am/learning/spring/springpetclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/i/am/learning/spring/springpetclinic/SpringPetClinicApplication.java
@@ -1,4 +1,4 @@
-package springpetclinic;
+package i.am.learning.spring.springpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
