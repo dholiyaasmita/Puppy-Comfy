980e525ed9eb8633add8fe077b3cb0daa26234a1
Asmi <maredia1990@gmail.com>
2020-12-03 15:01:05 -0500

    Adding BaseEntity. Closes #10.

---
+++ pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/BaseEntity.java
@@ -0,0 +1,15 @@
+package i.am.learning.spring.springpetclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package i.am.learning.spring.springpetclinic.model;
 
-public class  PetType {
+public class  PetType extends BaseEntity{
     private String name;
 
     public String getName() {
