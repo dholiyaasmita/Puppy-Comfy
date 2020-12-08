886f1f6f0bde05884ffae35136d923041904ba0c
Asmi <asmi.katrodiya@gmail.com>
2020-12-08 17:51:13 -0500

    Fixed Broken Links. Closes #45.

--- pet-clinic-web/src/main/java/i/am/learning/spring/springpetclinic/controllers/ownerController.java
+++ pet-clinic-web/src/main/java/i/am/learning/spring/springpetclinic/controllers/ownerController.java
@@ -21,4 +21,9 @@ public class ownerController {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+        return "notimplemented";
+    }
 }
