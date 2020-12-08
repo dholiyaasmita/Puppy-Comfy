636a0fae53ba33f3cd28f38c8d475ae2029f51c7
Asmi <asmi.katrodiya@gmail.com>
2020-12-08 16:35:05 -0500

    added Specialities, and vets entities. Closes #27.

--- pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/Vet.java
@@ -1,6 +1,16 @@
 package i.am.learning.spring.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+    private Set<Speciality> speciality;
 
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
