87d5bd1f9d8faaf3516696ddebc3ee69ff81af95
Asmita Dholiya <asmi.katrodiya@gmail.com>
2020-12-21 15:24:42 -0500

    Updated Vet to use JPA annotations.Closes #37.

--- pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/i/am/learning/spring/springpetclinic/model/Vet.java
@@ -1,10 +1,15 @@
 package i.am.learning.spring.springpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",joinColumns = @JoinColumn(name = "vet_id"),
+    inverseJoinColumns = @JoinColumn(name = "specialist_id"))
     private Set<Speciality> speciality = new HashSet<>();
 
     public Set<Speciality> getSpeciality() {
