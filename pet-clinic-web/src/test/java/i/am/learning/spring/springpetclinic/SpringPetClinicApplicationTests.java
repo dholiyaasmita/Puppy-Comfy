0936086612d92c73a6d31e4f4374d89a7c1c2dcc
Asmita Dholiya <asmi.katrodiya@gmail.com>
2020-12-22 16:03:04 -0500

    Upgraded to Junit 5.Closes #4.

---
+++ pet-clinic-web/src/test/java/i/am/learning/spring/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package i.am.learning.spring.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
